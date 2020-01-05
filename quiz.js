814502be908ba177f2a61263098c1745071ab0ae
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-04 18:42:54 -0700

    restructured html for unordered list to target

--- quiz.js
+++ quiz.js
@@ -1,62 +1,46 @@
-//document.getElementById("clickButton").addEventListener("click", quizFunction());
-   
-
 var score = 0;
 
 var questions = [
-        {
-          title: "Commonly used data types DO NOT include:",
-          choices: ["strings", "booleans", "alerts", "numbers"],
-          answer: "alerts"
-        },
-        {
-          title: "The condition in an if / else statement is enclosed within ____.",
-          choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-          answer: "parentheses"
-        },
-        {
-          title: "What year was Javascript initially created?",
-          choices: ["2000", "1825", "1995", "1992"],
-          answer: "1992"
-        },
-        {
-          title: "Which symbol is used to create single line comments in Javascript?",
-          choices: ["//", "question marks", "parentheses", "$$"],
-          answer: "//"
-        },
-        {
-          title: "What text do you need to start building a for-loop?",
-          choices: ["begin", "for", "start text", "none, just use square brackets"],
-          answer: "for"
-        },
-        
-      ];
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "What year was Javascript initially created?",
+    choices: ["2000", "1825", "1995", "1992"],
+    answer: "1992"
+  },
+  {
+    title: "Which symbol is used to create single line comments in Javascript?",
+    choices: ["//", "question marks", "parentheses", "$$"],
+    answer: "//"
+  },
+  {
+    title: "What text do you need to start building a for-loop?",
+    choices: ["begin", "for", "start text", "none, just use square brackets"],
+    answer: "for"
+  },
+
+];
 
 
-      function quizFunction(){
-        var doc = document.getElementById("myQuestions");
-        //for
-        doc.innerHTML = questions[0].title;  
-        for (var i = 0; i < 4; i++){
-          var btn = document.createElement("BUTTON");
-          btn.innerHTML = questions[0].choices[i];
-          doc.appendChild(document.createElement("br"));
-          doc.appendChild(btn);
-        }
-       
-      }
+function quizFunction() {
+  var doc = document.getElementById("myQuestions");
+  //for
+  doc.innerHTML = questions[0].title;
+  for (var i = 0; i < 4; i++) {
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = questions[0].choices[i];
+    doc.appendChild(document.createElement("br"));
+    doc.appendChild(btn);
+  }
+
+}
 
 
-
-
-//Options?
-//If you put a variable to .value it makes the input what you get from the user into a value you can use
-//Abby's suggestion of currentQuestion++ instead of for loop
-
-// var choices = [];
-// var currentQuestion = 0
-//     for (currentQuestion = 0; answer < choices.length; currentQuestion++) {
-//         text += choices[i] + "";
-//       }
-
-//Unordered list within form <ul id = "question-list">; li id = "0"?
