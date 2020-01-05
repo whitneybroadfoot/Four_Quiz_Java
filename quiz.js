02ebe2c3354b7e80c221fb0c724c1c90b4fb31bf
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-05 12:09:53 -0700

    variables and answers for if else

--- quiz.js
+++ quiz.js
@@ -1,46 +1,87 @@
-var score = 0;
+//main variables live at top of js
+var ul = document.getElementById('ul');
+var btn = document.getElementById('button');
+var scoreCard = document.getElementById('scoreCard');
+var quizBox = document.getElementById('questionBox');
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
 
-var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
+//questions for grabbing and looping
+var app = {
+  questions: [
+
+    {
+      title: "Commonly used data types DO NOT include",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: 2
+    },
+
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: 2
+    },
+
+    {
+      title: "What year was Javascript initially created?",
+      choices: ["2000", "1825", "1995", "1992"],
+      answer: 3
+    },
+
+    {
+      title: "Which symbol is used to create single line comments in Javascript?",
+      choices: ["//", "question marks", "parentheses", "$$"],
+      answer: 0
+    },
+
+    {
+      title: "What text do you need to start building a for-loop?",
+      choices: ["begin", "for", "start text", "none, just use square brackets"],
+      answer: 1
+    }
+
+  ],
+
+//options for choosing
+  index: 0,
+  load: function () {
+    if (this.index <= this.questions.length - 1) {
+      quizBox.innerHTML = this.index + 1 + ". " + this.questions[this.index].title;
+      one.innerHTML = this.questions[this.index].choices[0];
+      two.innerHTML = this.questions[this.index].choices[1];
+      three.innerHTML = this.questions[this.index].choices[2];
+      four.innerHTML = this.questions[this.index].choices[3];
+      this.scoreCard();
+    }
+    else {
+
+      quizBox.innerHTML = "Quiz Completed"
+      one.style.display = "none";
+      two.style.display = "none";
+      three.style.display = "none";
+      four.style.display = "none";
+      five.style.display = "none";
+
+    }
   },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
+  next: function () {
+    this.index++;
+    this.load();
   },
-  {
-    title: "What year was Javascript initially created?",
-    choices: ["2000", "1825", "1995", "1992"],
-    answer: "1992"
+  check: function (ele) {
+
+    var id = ele.id.split('');
+
+    if (id[id.length - 1] == this.questions[this.index].answer) {
+      this.score++;
+      ele.className = "correct";
+      ele.innerHTML = "Correct";
+      this.scoreCard();
+    }
+    else {
+      ele.className = "wrong";
+      ele.innerHTML = "Wrong";
+    }
   },
-  {
-    title: "Which symbol is used to create single line comments in Javascript?",
-    choices: ["//", "question marks", "parentheses", "$$"],
-    answer: "//"
-  },
-  {
-    title: "What text do you need to start building a for-loop?",
-    choices: ["begin", "for", "start text", "none, just use square brackets"],
-    answer: "for"
-  },
-
-];
-
-
-function quizFunction() {
-  var doc = document.getElementById("myQuestions");
-  //for
-  doc.innerHTML = questions[0].title;
-  for (var i = 0; i < 4; i++) {
-    var btn = document.createElement("BUTTON");
-    btn.innerHTML = questions[0].choices[i];
-    doc.appendChild(document.createElement("br"));
-    doc.appendChild(btn);
-  }
-
-}
-
-
